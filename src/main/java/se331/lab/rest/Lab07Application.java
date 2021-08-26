4237e88dcb389288aa08e1f02fb731168f5f9806
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-08-26 19:37:51 +0700

    4.3

--- src/main/java/se331/lab/rest/Lab07Application.java
+++ src/main/java/se331/lab/rest/Lab07Application.java
@@ -2,6 +2,10 @@ package se331.lab.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab07Application {
@@ -10,4 +14,13 @@ public class Lab07Application {
         SpringApplication.run(Lab07Application.class, args);
     }
 
+@Bean
+public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer(){
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/events").allowedOrigins("http://localhost:3000");
+            }
+        };
+}
 }
