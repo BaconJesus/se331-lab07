b202f29db9d02552a347551d7473d811b9df7d05
BaconJesus <Kadnoknart_C@cmu.ac.th>
2021-08-26 16:18:28 +0700

    2.10

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,11 +1,14 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import se331.lab.rest.entity.Event;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Controller
 public class EventController {
@@ -80,5 +83,9 @@ public class EventController {
                 .petAllowed(false)
                 .organizer("Brody Kill")
                 .build());
+    }
+    @GetMapping("events")
+    public ResponseEntity<?> getEventLists(){
+        return ResponseEntity.ok(eventList);
     }
 }
